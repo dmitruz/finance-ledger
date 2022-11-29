cc2abd1292a8e925c5c302980412ba6833e0e5ab
dmitruz <dmitruz970@gmail.com>
2022-11-29 18:45:41 +0200

    finished js

---
+++ src/js/header.js
@@ -0,0 +1,24 @@
+
+// <div id="myDIV">
+//   <button class="btn">1</button>
+//   <button class="btn active">2</button>
+//   <button class="btn">3</button>
+//   <button class="btn">4</button>
+//   <button class="btn">5</button>
+// </div>
+
+// <script>
+// // Add active class to the current button (highlight it)
+// var header = document.getElementById("myDIV");
+// var btns = header.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   current[0].className = current[0].className.replace(" active", "");
+//   this.className += " active";
+//   });
+// }
+// </script>
+
+//https://stackoverflow.com/questions/38990163/how-can-i-add-and-remove-an-active-class-to-an-element-in-pure-javascript
+
